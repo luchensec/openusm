1b59b52b05d15e04e435a6486878caec67620a4a
xingfuxiaohai <xingfu_xiaohai@163.com>
2020-03-24 22:44:23 +0800

    增加IndexControll和SystemConfigController

---
+++ web/src/main/java/com/openusm/web/system/controller/SystemConfigController.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2019-2020
+ */
+package com.openusm.web.system.controller;
+
+import com.openusm.web.system.service.SystemConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * 2020/3/24 22:42
+ * @author xingfu_xiaohai@163.com
+ * @since 1.0.0
+ *
+ */
+@RestController
+public class SystemConfigController {
+
+    @Autowired
+    private SystemConfigService systemConfigService;
+}
