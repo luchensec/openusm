dfe4010eef8142867875953ad97328ed55983f2a
xingfuxiaohai <xingfu_xiaohai@163.com>
2020-03-29 23:02:25 +0800

    完善启动类配置

---
+++ collector/src/main/java/com/openusm/collector/AppStart.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C), 2019-2020
+ */
+package com.openusm.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * 2020/3/9 20:34
+ * @author xingfu_xiaohai@163.com
+ * @since 1.0.0
+ *
+ */
+@SpringBootApplication
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class, args);
+    }
+}
