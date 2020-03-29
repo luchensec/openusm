19e6e75e48009154c8feebb9463ac71f0a9bb2c1
xingfuxiaohai <xingfu_xiaohai@163.com>
2020-03-29 23:13:11 +0800

    增加放行验证码请求

--- web/src/main/java/com/openusm/web/config/shiro/ShiroConfigutation.java
+++ web/src/main/java/com/openusm/web/config/shiro/ShiroConfigutation.java
@@ -50,9 +50,8 @@ public class ShiroConfigutation {
         //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了，登出后跳转配置的loginUrl
         filterChainDefinitionMap.put("/logout", "logout");
         // 配置不会被拦截的链接 顺序判断
-        filterChainDefinitionMap.put("/static/**", "anon");
-        filterChainDefinitionMap.put("/ajaxLogin", "anon");
-        filterChainDefinitionMap.put("/favicon.ico", "anon");
+        filterChainDefinitionMap.put("/generateVerifyCode", "anon");
+        filterChainDefinitionMap.put("/common/**", "anon");
         filterChainDefinitionMap.put("/login", "anon");
         filterChainDefinitionMap.put("/**", "authc");
         //配置shiro默认登录界面地址，前后端分离中登录界面跳转应由前端路由控制，后台仅返回json数据
